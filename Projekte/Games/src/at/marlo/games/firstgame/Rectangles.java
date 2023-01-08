efe1d9b4068e3bdd6a7b35bb3ae06ed0e355f91e
Marloschi <marlo.mitterlehner@gmx.at>
2023-01-08 22:52:50 +0100

    Rectangle Speed

--- Projekte/Games/src/at/marlo/games/firstgame/Rectangles.java
+++ Projekte/Games/src/at/marlo/games/firstgame/Rectangles.java
@@ -6,8 +6,9 @@ import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
 
-    private int x;
-    private int y;
+    private float x;
+    private float y;
+    private float speed;
     public Rectangles(String title) {
         super(title);
     }
@@ -15,16 +16,12 @@ public class Rectangles extends BasicGame {
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
         this.x = 100;
+        this.speed = 10.0f;
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-        this.x++;
-        this.y++;
-        if (this.x>800){
-            this.x = 0;
-            this.y = 0;
-        }
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+        this.x += (float)delta/this.speed;
     }
 
     @Override
